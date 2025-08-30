f160d1126c5526d6c068979e74301fbc3bdf22c9
Shiyam <shiyamlawrence@redantlabs.com>
2025-08-30 16:31:43 +0530

    'initial'

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -21,7 +21,7 @@ export default function Navbar() {
     { name: 'Home', href: '/' },
     { name: 'About', href: '/about' },
     { name: 'Service', href: '/service' },
-    { name: 'Projects', href: '#' },
+    { name: 'Projects', href: 'projects' },
     { name: 'Contact', href: '/contact' },
   ];
 
